5c28b8517146ddd6ad2ef8072c881b9d8e488f73
Devansh <myworld.devansh@gmail.com>
2018-03-04 18:27:33 +0530

    Some Addition In Config File

--- admin/includes/my_config.php
+++ admin/includes/my_config.php
@@ -1,10 +1,17 @@
 <?php
 /**************************************************
+ * Error Reporting
+ **************************************************/
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+/**************************************************
  * Database Configuration
  **************************************************/
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456');
 define('DB_NAME', 'blog');
 
 /**************************************************
