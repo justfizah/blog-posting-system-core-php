55135d9b56f4652b6b88b2cac6ece591c02a48dd
Devansh <myworld.devansh@gmail.com>
2018-03-04 23:25:14 +0530

    Some Files Are Ignored And Sample Configuration Added

---
+++ admin/includes/sample_config.php
@@ -0,0 +1,26 @@
+<?php
+/**************************************************
+ * Rename the file to 'my_config.php' to properly
+ * link the variables.
+ **************************************************/
+
+/**************************************************
+ * Error Reporting
+ **************************************************/
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+/**************************************************
+ * Database Configuration
+ **************************************************/
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+/**************************************************
+ * Normal Configuration
+ **************************************************/
+date_default_timezone_set('Asia/Kolkata');
+?>
